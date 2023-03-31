be9bed1d7249b35e391591e8ca20ba1a2331d70c
Tora Mullings <20993137+djunga@users.noreply.github.com>
2023-03-30 22:45:53 -0400

    ny 511 events draft

--- app.py
+++ app.py
@@ -1,93 +1,77 @@
 from dash import Dash, dcc, html, Input, Output
 import pandas as pd
 import plotly.express as px
+from datetime import date
+from plotly_calplot import calplot
 
 app = Dash(__name__)
 
-#url = 'https://data.cityofnewyork.us/resource/nwxe-4ae8.json?$limit=683788'
-frames=[]
-for i in range (1,13):
-    url = "https://raw.githubusercontent.com/djunga/DATA608/main/tree"+str(i)+".csv"
-    frames.append(pd.read_csv(url))
+px.set_mapbox_access_token(open(".mapbox_token").read())
 
-df2 = pd.concat(frames)[["spc_common", "borough", "health", "steward"]]
-df1 = df2[["spc_common", "borough", "health"]] # question 1
+def getDateData(start_date, end_date):
+    url = ('https://data.ny.gov/resource/ah74-pg4w.json?' +\
+        '$limit=200&$offset=0' +\
+        '&$select=*' +\
+        '&$where=date_trunc_ymd(create_time) between \"' + start_date + '\" and \"' + end_date + '\"'
+        ).replace(' ', '%20')
+    test = pd.read_json(url)
+    return test
 
-def getData1(x):
-    boroughs=["Brooklyn", "Bronx", "Manhattan", "Queens", "Staten Island"]
-    conditions=["Good", "Fair", "Poor"]
-    data=[]
-    for b in boroughs:
-        arr = []
-        for c in conditions:
-            prop=len(x.loc[(x.borough==b) & (x.health==c)]) / len(x)
-            data.append([b,c,round(prop, 2)])
-    y = pd.DataFrame(data, columns=["borough", "condition", "prop"])
+def initDatePicker():
+    url = ('https://data.ny.gov/resource/ah74-pg4w.json?' +\
+    '&$select=date_trunc_ymd(max(create_time)) as create_time'
+    ).replace(' ', '%20')
+    full_date=pd.read_json(url).create_time.item()
+    return date(full_date.year, full_date.month, full_date.day)
 
-    return y
-
-def getData2(x):
-    x = x.dropna(subset=["steward", "spc_common"])
-    data=[]
-    health=["Good", "Fair", "Poor"]
-    steward=["4orMore", "3or4", "1or2", "None"]
-    for h in health:
-        perc = []
-        for s in steward:
-            perc.append(len(x.loc[(x.steward==s) & (x.health==h)]) / len(x))
-        data.append(perc)
-    return data
+my_url='sales_data_sample.csv'
+df = pd.read_csv(my_url, encoding_errors='ignore')
+df['ORDERDATE'] = pd.to_datetime(df['ORDERDATE'])
+pl1 = calplot(df, x="ORDERDATE", y="SALES", dark_theme=True)
+# The only requirement for you to use this plot is that you have 
+# a Pandas dataframe with a date column (see pandas to_datetime docs) 
+# and a value column which will be used as the value we’ll plot.
 
 app.layout = html.Div([
     html.Div([
-        dcc.Dropdown(list(df2.spc_common.unique()), value='pin oak', id="dropdown"),
-        dcc.Graph(id="graph1")
+        # dcc.Graph(id="graph1"),
+        # dcc.DatePickerRange(
+        #     id='my-date-picker-range',
+        #     month_format='MMM Do, YYYY',
+        #     display_format='MMM Do, YYYY',
+        #     min_date_allowed=date(2012, 11, 6),
+        #     max_date_allowed=date(2020, 4, 20),
+        #     initial_visible_month= initDatePicker(),#date(2020, 4, 20),
+        #     start_date=date(2020, 1, 20),
+        #     end_date=date(2020, 4, 20)
+        # ),
+        dcc.Graph(id="graph2", figure=pl1),
     ], style={'padding': 10, 'flex': 1}),
-    html.Div([
-        dcc.RadioItems(list(df2.borough.unique()), value="Brooklyn", id="radio"),
-        dcc.Graph(id="graph2")
-    ], style={'padding': 10, 'flex': 1})
 ], style={'display': 'flex', 'flex-direction': 'row'})
 
-# Graph 1, the bar plot
 @app.callback(
-    Output(component_id='graph1', component_property='figure'),
-    Input(component_id='dropdown', component_property="value")
+        Output(component_id='graph1', component_property='figure'),
+        Input('my-date-picker-range', 'start_date'),
+        Input('my-date-picker-range', 'end_date')
 )
-def update_barplot(species):
-    filtered=df1[df2.spc_common == species]
-    fig = px.bar(getData1(filtered), x="borough", y="prop", color="condition", 
-    range_y=[0.0,1.0],
-    barmode="group",
-    template="plotly_dark",
-    labels = {
-        "borough": "borough",
-        "prop": "Proportion",
-        "condition": "Condition"
-    },
-    title=species.title() + " Health in NYC")
-
-    fig.update_layout(xaxis=dict(showgrid=False),
-                yaxis=dict(showgrid=False)
+def update_map(start_date, end_date):
+    fig = px.scatter_mapbox(data_frame=getDateData(start_date, end_date), lat='latitude', lon='longitude', 
+        opacity=0.5, 
+        hover_name="event_type", 
+        hover_data=["organization_name","state", "county", "create_time"], 
+        zoom=5,
+        mapbox_style="dark"
+    )
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0},  # remove the white gutter between the frame and map
+            # hover appearance
+            hoverlabel=dict( 
+            bgcolor="white",     # white background
+            font_size=16,        # label font size
+            font_family="Inter") # label font
     )
 
     return fig
 
-# Graph 2, the heatplot
-@app.callback(
-    Output(component_id='graph2', component_property='figure'),
-    Input(component_id='radio', component_property="value"),
-    Input(component_id='dropdown', component_property="value")
-)
-def update_heatplot(borough, species):
-    filtered = df2.loc[(df2.borough==borough) & (df2.spc_common==species)]
-    health=["Good", "Fair", "Poor"]
-    steward=["4orMore", "3or4", "1or2", "None"]
-    fig = px.imshow(getData2(filtered),
-                labels=dict(x="Steward", y="Health", color="Percent of Trees"),
-                x=steward,y=health)
-    fig.update_xaxes(side="top")
-    return fig
-
 if __name__ == "__main__":
     app.run_server(debug=True)
+    
